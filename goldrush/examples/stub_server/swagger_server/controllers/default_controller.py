a58204973526665a4cd378644b6c4363f91edde6
Dmitry Kashtanov <Dmitry.Kashtanov@piano.io>
2021-02-21 14:03:07 +0300

    Generated code for HighLoad Cup 2021 API v1.0.0.

---
+++ goldrush/examples/stub_server/swagger_server/controllers/default_controller.py
@@ -0,0 +1,104 @@
+import connexion
+import six
+
+from swagger_server.models.area import Area  # noqa: E501
+from swagger_server.models.balance import Balance  # noqa: E501
+from swagger_server.models.dig import Dig  # noqa: E501
+from swagger_server.models.error import Error  # noqa: E501
+from swagger_server.models.license import License  # noqa: E501
+from swagger_server.models.license_list import LicenseList  # noqa: E501
+from swagger_server.models.report import Report  # noqa: E501
+from swagger_server.models.treasure_list import TreasureList  # noqa: E501
+from swagger_server.models.wallet import Wallet  # noqa: E501
+from swagger_server import util
+
+
+def cash(body):  # noqa: E501
+    """cash
+
+    Exchange provided treasure for money. # noqa: E501
+
+    :param body: Treasure for exchange.
+    :type body: dict | bytes
+
+    :rtype: Wallet
+    """
+    if connexion.request.is_json:
+        body = str.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def dig(body):  # noqa: E501
+    """dig
+
+    Dig at given point and depth, returns found treasures. # noqa: E501
+
+    :param body: License, place and depth to dig.
+    :type body: dict | bytes
+
+    :rtype: TreasureList
+    """
+    if connexion.request.is_json:
+        body = Dig.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def explore_area(body):  # noqa: E501
+    """explore_area
+
+    Returns amount of treasures in the provided area at full depth. # noqa: E501
+
+    :param body: Area to be explored.
+    :type body: dict | bytes
+
+    :rtype: Report
+    """
+    if connexion.request.is_json:
+        body = Area.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def get_balance():  # noqa: E501
+    """get_balance
+
+    Returns a current balance. # noqa: E501
+
+
+    :rtype: Balance
+    """
+    return 'do some magic!'
+
+
+def health_check():  # noqa: E501
+    """health_check
+
+    Returns 200 if service works okay. # noqa: E501
+
+
+    :rtype: Dict[str, object]
+    """
+    return 'do some magic!'
+
+
+def issue_license(body=None):  # noqa: E501
+    """issue_license
+
+    Issue a new license. # noqa: E501
+
+    :param body: Amount of money to spend for a license. Empty array for get free license. Maximum 10 active licenses
+    :type body: List[]
+
+    :rtype: License
+    """
+    return 'do some magic!'
+
+
+def list_licenses():  # noqa: E501
+    """list_licenses
+
+    Returns a list of issued licenses. # noqa: E501
+
+
+    :rtype: LicenseList
+    """
+    return 'do some magic!'
---
+++ goldrush/examples/stub_server/swagger_server/test/test_default_controller.py
@@ -0,0 +1,115 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.area import Area  # noqa: E501
+from swagger_server.models.balance import Balance  # noqa: E501
+from swagger_server.models.dig import Dig  # noqa: E501
+from swagger_server.models.error import Error  # noqa: E501
+from swagger_server.models.license import License  # noqa: E501
+from swagger_server.models.license_list import LicenseList  # noqa: E501
+from swagger_server.models.report import Report  # noqa: E501
+from swagger_server.models.treasure_list import TreasureList  # noqa: E501
+from swagger_server.models.wallet import Wallet  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestDefaultController(BaseTestCase):
+    """DefaultController integration test stubs"""
+
+    def test_cash(self):
+        """Test case for cash
+
+        
+        """
+        body = 'body_example'
+        response = self.client.open(
+            '/All-Cups/highloadcup/raw/main/goldrush/swagger.yaml/cash',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_dig(self):
+        """Test case for dig
+
+        
+        """
+        body = Dig()
+        response = self.client.open(
+            '/All-Cups/highloadcup/raw/main/goldrush/swagger.yaml/dig',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_explore_area(self):
+        """Test case for explore_area
+
+        
+        """
+        body = Area()
+        response = self.client.open(
+            '/All-Cups/highloadcup/raw/main/goldrush/swagger.yaml/explore',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_get_balance(self):
+        """Test case for get_balance
+
+        
+        """
+        response = self.client.open(
+            '/All-Cups/highloadcup/raw/main/goldrush/swagger.yaml/balance',
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_health_check(self):
+        """Test case for health_check
+
+        
+        """
+        response = self.client.open(
+            '/All-Cups/highloadcup/raw/main/goldrush/swagger.yaml/health-check',
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_issue_license(self):
+        """Test case for issue_license
+
+        
+        """
+        body = [56]
+        response = self.client.open(
+            '/All-Cups/highloadcup/raw/main/goldrush/swagger.yaml/licenses',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_list_licenses(self):
+        """Test case for list_licenses
+
+        
+        """
+        response = self.client.open(
+            '/All-Cups/highloadcup/raw/main/goldrush/swagger.yaml/licenses',
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
